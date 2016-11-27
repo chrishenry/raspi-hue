4dc615f215a0a59cfb6f7b54b8888693493d1463
Chris Henry <henry.christopher@gmail.com>
2016-11-26 22:17:37 -0500

    chg: Get colors

--- raspi-hue/camera.py
+++ raspi-hue/camera.py
@@ -3,9 +3,7 @@ from time import sleep
 from picamera import PiCamera
 from PIL import Image
 
-def get_image_colors():
-
-    image = get_image_buffer()
+def get_image_colors(image):
 
     print image
     print image.show()
@@ -25,4 +23,14 @@ def get_image():
     camera.capture(my_stream, 'jpeg')
     camera.close()
 
+    my_stream.seek(0)
+
     return Image.open(my_stream)
+
+
+if __name__ == '__main__':
+    image = get_image()
+    print image
+    colors = image.getcolors(maxcolors=1000000)
+    colors.sort(key=lambda x: x[0])
+    print colors[-10:]
